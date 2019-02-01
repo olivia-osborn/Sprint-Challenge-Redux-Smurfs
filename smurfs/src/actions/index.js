e85764c55dbc7a3ecb53aa13c031cf3b1a39ae93
Olivia Osborn <olioz95@gmail.com>
2019-02-01 12:08:08 -0500

    created addNewSmurf action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export const FETCHING_SMURFS_START = "FETCHING_SMURFS_START";
 export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
 export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+export const ADDING_SMURF_START = "ADDING_SMURF_START";
+export const ADDING_SMURF_SUCCESS = "ADDING_SMURF_SUCCESS";
+export const ADDING_SMURF_FAILURE = "ADDING_SMURF_FAILURE";
 
 /* 
   Action Types Go Here!
@@ -12,18 +15,28 @@ export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
 export const getSmurfs = () => dispatch => {
   dispatch({type: FETCHING_SMURFS_START});
   axios
-    .get("http://localhost:3333/smfurfs")
+    .get("http://localhost:3333/smurfs")
     .then(response => {
-      console.log(response)
       dispatch({type: FETCHING_SMURFS_SUCCESS, payload: response.data})
     })
     .catch(err => {
-      console.log(err)
       dispatch({type: FETCHING_SMURFS_FAILURE, payload: err})
     })
 }
 
-
+export const addNewSmurf = newSmurf => dispatch => {
+  dispatch({type: ADDING_SMURF_START});
+  axios
+    .post("http://localhost:3333/smurfs", newSmurf)
+    .then(response => {
+      console.log(response)
+      dispatch({type: ADDING_SMURF_SUCCESS, payload: response.data})
+    })
+    .catch(err => {
+      console.log(err)
+      dispatch({type: ADDING_SMURF_FAILURE, payload: err})
+    })
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
